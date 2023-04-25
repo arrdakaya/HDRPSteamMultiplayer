821610ccf957803bda955e3a7284bbab427d39c4
Arda Kaya <arrdakaya@hotmail.com>
2023-04-25 16:50:29 +0300

    Door mechanics

--- Assets/Scripts/Player/PlayerPickUp.cs
+++ Assets/Scripts/Player/PlayerPickUp.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Mirror;
 using TMPro;
+using Unity.VisualScripting;
 
 public class PlayerPickUp : NetworkBehaviour
 {
@@ -16,55 +17,100 @@ public class PlayerPickUp : NetworkBehaviour
     public string[] itemTitles;
 
     private int objID = 0;
+    private AudioSource audioPlayer;
+    public GameObject doorMessageObj;
+    public TextMeshProUGUI doorMessage;
+    public AudioClip[] pickupSounds;
 
     private void Start()
     {
-        pickupPanel.SetActive(false);
+        audioPlayer = GetComponent<AudioSource>();
+        doorMessageObj.SetActive(false);
     }
     private void Update()
     {
         if (!isLocalPlayer) return;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
         if (Input.GetKeyDown(KeyCode.T))
         {
             CutsceneManager.Instance.CmdSwitchToCutscene();
         }
-        if (Physics.SphereCast(transform.position,0.5f,transform.forward,out hit, 30.0f, includeLayer))
+        if (Physics.Raycast(ray, out hit, 2, includeLayer))
         {
-            if(Vector3.Distance(transform.position,hit.transform.position) < 6)
+            
+            if (hit.transform.gameObject.CompareTag("Item"))
             {
-                if (hit.transform.gameObject.CompareTag("Item"))
+                pickupPanel.SetActive(true);
+                objID = (int)hit.transform.gameObject.GetComponent<ItemTypes>().chooseItem;
+                mainImage.sprite = itemIcons[objID];
+                mainTitle.text = itemTitles[objID];
+                Debug.Log(hit.collider.name);
+                if (Input.GetKeyDown(KeyCode.E))
                 {
-                    pickupPanel.SetActive(true);
-                    objID = (int)hit.transform.gameObject.GetComponent<ItemTypes>().chooseItem;
-                    mainImage.sprite = itemIcons[objID];
-                    mainTitle.text = itemTitles[objID];
-                    Debug.Log(hit.collider.name);
-                    if (Input.GetKeyDown(KeyCode.E))
-                    {
                       
-                        SaveScript.itemAmts[objID]++;
-                        CmdDestroyItem(hit.transform.gameObject);
+                    SaveScript.itemAmts[objID]++;
+                    CmdDestroyItem(hit.transform.gameObject);
+                }
+            }
+              
+             else if (hit.transform.gameObject.CompareTag("Door"))
+             {
+                SaveScript.doorObject = hit.transform.gameObject;
+                objID = (int)hit.transform.gameObject.GetComponent<DoorType>().chooseDoor;
+                doorMessageObj.SetActive(true);
+                doorMessage.text = hit.transform.gameObject.GetComponent<DoorType>().message;
+
+                if (SaveScript.itemAmts[2] != 0)
+                {
+                    hit.transform.gameObject.GetComponent<DoorType>().locked = false;
+                }
+
+                if(hit.transform.gameObject.GetComponent<DoorType>().locked == true)
+                {
+                        doorMessage.text = "Locked. Find the key";
+                }
+
+                if(Input.GetKeyDown(KeyCode.E) && hit.transform.gameObject.GetComponent<DoorType>().locked == false)
+                {
+                    audioPlayer.clip = pickupSounds[0];
+                    audioPlayer.Play();
+
+                    if (hit.transform.gameObject.GetComponent<DoorType>().opened == false)
+                    {
+                            hit.transform.gameObject.GetComponent<DoorType>().message = " Press E to close the door";
+                            hit.transform.gameObject.GetComponent<DoorType>().opened = true;
+                            hit.transform.gameObject.GetComponent<Animator>().SetTrigger("Open");
+                    }
+                    else if (hit.transform.gameObject.GetComponent<DoorType>().opened == true)
+                    {
+                            hit.transform.gameObject.GetComponent<DoorType>().message = " Press E to open the door";
+                            hit.transform.gameObject.GetComponent<DoorType>().opened = false;
+                            hit.transform.gameObject.GetComponent<Animator>().SetTrigger("Close");
                     }
                 }
-                
             }
-           
+               
         }
         else
         {
             if (pickupPanel.activeSelf == true)
             {
-                Debug.Log("panelkapan");
                 pickupPanel.SetActive(false);
+            }
+            if(doorMessageObj.activeSelf == true)
+            {
+                doorMessageObj.SetActive(false);
+                SaveScript.doorObject = null;
             }
 
         }
-
-
-
     }
+        
+        
+           
+              
     [Command(requiresAuthority = false)]
     void CmdDestroyItem(GameObject obj)
     {
--- Assets/Scripts/Player/SaveScript.cs
+++ Assets/Scripts/Player/SaveScript.cs
@@ -7,5 +7,5 @@ public class SaveScript : MonoBehaviour
     public static int itemID;
     //public static bool[] itemsPickedUp = new bool[2];
     public static int[] itemAmts = new int[4];
-  
+    public static GameObject doorObject;
 }
