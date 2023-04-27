f43f93a1f2baa8c9bca514c8544de1b6d10e6f4b
Arda Kaya <arrdakaya@hotmail.com>
2023-04-27 17:30:53 +0300

    Sword added

--- Assets/Scripts/Player/PlayerPickUp.cs
+++ Assets/Scripts/Player/PlayerPickUp.cs
@@ -10,11 +10,19 @@ public class PlayerPickUp : NetworkBehaviour
     private RaycastHit hit;
     public LayerMask includeLayer;
     public GameObject pickupPanel;
+    public GameObject weaponParent;
 
+    [Header("Item Features")]
     public Image mainImage;
     public Sprite[] itemIcons;
     public TextMeshProUGUI mainTitle;
     public string[] itemTitles;
+
+
+    [Header("Weapon Features")]
+    public Sprite[] weaponIcons;
+    public string[] weaponTitles;
+    private GameObject currentWeapon;
 
     private int objID = 0;
     private AudioSource audioPlayer;
@@ -32,27 +40,47 @@ public class PlayerPickUp : NetworkBehaviour
         if (!isLocalPlayer) return;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-       
+
         if (Physics.Raycast(ray, out hit, 2, includeLayer))
         {
-            
+
             if (hit.transform.gameObject.CompareTag("Item"))
             {
                 pickupPanel.SetActive(true);
                 objID = (int)hit.transform.gameObject.GetComponent<ItemTypes>().chooseItem;
                 mainImage.sprite = itemIcons[objID];
                 mainTitle.text = itemTitles[objID];
-                Debug.Log(hit.collider.name);
                 if (Input.GetKeyDown(KeyCode.E))
                 {
-                      
+
                     SaveScript.itemAmts[objID]++;
                     CmdDestroyItem(hit.transform.gameObject);
                 }
             }
-              
-             else if (hit.transform.gameObject.CompareTag("Door"))
-             {
+            else if (hit.transform.gameObject.CompareTag("Weapon"))
+            {
+                pickupPanel.SetActive(true);
+                objID = (int)hit.transform.gameObject.GetComponent<WeaponTypes>().chooseWeapon;
+                mainImage.sprite = weaponIcons[objID];
+                mainTitle.text = weaponTitles[objID];
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if (currentWeapon == null)
+                    {
+                        Pickup();
+                    }
+                    else
+                    {
+                        Drop();
+                        if (currentWeapon == null)
+                            Pickup();
+                    }
+                }
+            }
+
+
+            else if (hit.transform.gameObject.CompareTag("Door"))
+            {
                 SaveScript.doorObject = hit.transform.gameObject;
                 objID = (int)hit.transform.gameObject.GetComponent<DoorType>().chooseDoor;
                 doorMessageObj.SetActive(true);
@@ -63,31 +91,31 @@ public class PlayerPickUp : NetworkBehaviour
                     hit.transform.gameObject.GetComponent<DoorType>().locked = false;
                 }
 
-                if(hit.transform.gameObject.GetComponent<DoorType>().locked == true)
+                if (hit.transform.gameObject.GetComponent<DoorType>().locked == true)
                 {
-                        doorMessage.text = "Locked. Find the key";
+                    doorMessage.text = "Locked. Find the key";
                 }
 
-                if(Input.GetKeyDown(KeyCode.E) && hit.transform.gameObject.GetComponent<DoorType>().locked == false)
+                if (Input.GetKeyDown(KeyCode.E) && hit.transform.gameObject.GetComponent<DoorType>().locked == false)
                 {
                     audioPlayer.clip = pickupSounds[0];
                     audioPlayer.Play();
 
                     if (hit.transform.gameObject.GetComponent<DoorType>().opened == false)
                     {
-                            hit.transform.gameObject.GetComponent<DoorType>().message = " Press E to close the door";
-                            hit.transform.gameObject.GetComponent<DoorType>().opened = true;
-                            hit.transform.gameObject.GetComponent<Animator>().SetTrigger("Open");
+                        hit.transform.gameObject.GetComponent<DoorType>().message = " Press E to close the door";
+                        hit.transform.gameObject.GetComponent<DoorType>().opened = true;
+                        hit.transform.gameObject.GetComponent<Animator>().SetTrigger("Open");
                     }
                     else if (hit.transform.gameObject.GetComponent<DoorType>().opened == true)
                     {
-                            hit.transform.gameObject.GetComponent<DoorType>().message = " Press E to open the door";
-                            hit.transform.gameObject.GetComponent<DoorType>().opened = false;
-                            hit.transform.gameObject.GetComponent<Animator>().SetTrigger("Close");
+                        hit.transform.gameObject.GetComponent<DoorType>().message = " Press E to open the door";
+                        hit.transform.gameObject.GetComponent<DoorType>().opened = false;
+                        hit.transform.gameObject.GetComponent<Animator>().SetTrigger("Close");
                     }
                 }
-             }
-               
+            }
+
         }
         else
         {
@@ -95,17 +123,39 @@ public class PlayerPickUp : NetworkBehaviour
             {
                 pickupPanel.SetActive(false);
             }
-            if(doorMessageObj.activeSelf == true)
+            if (doorMessageObj.activeSelf == true)
             {
                 doorMessageObj.SetActive(false);
                 SaveScript.doorObject = null;
             }
 
         }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Drop();
+        }
     }
         
-        
-           
+
+    private void Pickup()
+    {
+        currentWeapon = hit.transform.gameObject;
+        currentWeapon.transform.GetComponent<Rigidbody>().isKinematic = true;
+        currentWeapon.transform.position = weaponParent.transform.position;
+        currentWeapon.transform.parent = weaponParent.transform;
+        currentWeapon.transform.localEulerAngles = new Vector3(75f, 0f, -90f);
+        currentWeapon.tag = "PlayerWeapon";
+    }
+    private void Drop()
+    {
+        if (currentWeapon != null)
+        {
+        currentWeapon.transform.parent = null;
+        currentWeapon.transform.GetComponent<Rigidbody>().isKinematic = false;
+        currentWeapon.tag = "Weapon";
+        currentWeapon = null;
+        }
+    }           
               
     [Command(requiresAuthority = false)]
     void CmdDestroyItem(GameObject obj)
---
+++ Assets/Scripts/WeaponTypes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponTypes : MonoBehaviour
+{
+    public enum typeOfWeapon
+    {
+        Sword
+    }
+    public typeOfWeapon chooseWeapon;
+}
